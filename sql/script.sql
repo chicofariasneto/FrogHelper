2faa9e1a674e766b3b35e733ab48fd4a8f6d628a
chicofariasneto <francisco.rneto@dcomp.ufs.br>
2020-07-05 13:58:43 -0300

    This commit changes script db

--- sql/script.sql
+++ sql/script.sql
@@ -1,17 +1,16 @@
 CREATE SCHEMA frog_helper
 
 CREATE TABLE frog_helper.group (
-    id_group INT NOT NULL,
+    id_group VARCHAR(50) NOT NULL,
     title_group VARCHAR(1000) NOT NULL,
 
     CONSTRAINT pk_frog_helper PRIMARY KEY (id_group)
 );
 
 CREATE TABLE frog_helper.user_group (
-    id_user INT NOT NULL,
-    id_group INT NOT NULL,
+    id_user VARCHAR(50) NOT NULL,
+    id_group VARCHAR(50) NOT NULL,
     username VARCHAR(120) NOT NULL,
 
-    CONSTRAINT pk_user_group PRIMARY KEY (id_user),
     CONSTRAINT fk_user_group FOREIGN KEY (id_group) REFERENCES frog_helper.group (id_group) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION
 );
